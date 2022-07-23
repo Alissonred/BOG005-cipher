73638b63808b3defcd45b588780ad45869e3cb24
Alissonred <108753902+Alissonred@users.noreply.github.com>
2022-07-23 09:18:28 -0500

    version 5

--- src/cipher.js
+++ src/cipher.js
@@ -6,7 +6,6 @@ encode(offset, string){
 let in_conv;
 let salida = [];
 let letra;
-
   
  for (var i = 0; i <= string.length-1; i++) {  /// tratar de usar for of, cuenta hasta leng-1 xq cuenta el 0 al get size
  let in_asci= string.charCodeAt(i); // get #asci de cada letra de string
@@ -40,24 +39,38 @@ decode(offset, string){
   let salida = [];
   let letra;
   
-    
    for (var i = 0; i <= string.length-1; i++) {  /// tratar de usar for of, cuenta hasta leng-1 xq cuenta el 0 al get size
    let in_asci= string.charCodeAt(i); // get #asci de cada letra de string
-      if (in_asci > 64 && in_asci < 91)  // evalua si mayusculas
+    //// evalua mayusculas
+      if (in_asci > 64 && in_asci < 91)  
       {
-      in_conv = (in_asci - 65 - offset) %26 + 65; // conv a nuevo alf, en otro #
-       }
-     else if (in_asci > 96 && in_asci < 123) // evalua si minusculas
-      {
-      in_conv = (in_asci - 97 - offset) %26 + 97; 
+        if ((in_asci - 65) - offset <0 ) {  //// si nueva posicion es negativa
+          in_conv = (((in_asci - 65 +26) - (offset-26) )) %26 + 65; // añada 26 a posicion de ref y quite a offset esos 26
+         }
+         else{
+        in_conv = ((in_asci - 65) - offset) %26 + 65; // conv a nuevo alf, en otro #
+         }
       }
-     else if (in_asci == 32 )     // evalua espacios
+     //// evalua minusculas
+      else if (in_asci > 96 && in_asci < 123) // evalua si minusculas
+      {
+        if ((in_asci - 97) - offset <0 ) {  //// si nueva posicion es negativa
+         in_conv = (((in_asci - 97 +26) - (offset-26) )) %26 + 97; // añada 26 a posicion de ref y quite a offset esos 26
+         }
+       else{
+        in_conv = (in_asci - 97 - offset) %26 + 97; 
+        }
+      }
+ 
+    //// evalua espacios
+     else if (in_asci == 32 )   
       {
       in_conv = in_asci; // mantiene espacio
       }
+      
      else { // otro caso
-       in_conv = in_asci;
-     }
+       in_conv = in_asci;}  
+    
       letra= String.fromCharCode(in_conv); // get letra a partir numero
       salida.push (letra);// va llenando array en cd ciclo */
    }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import cipher from './cipher.js'; /// traer objeto cifrado donde están las funciones ppales
-//console.log(cipher);
 
 // traigo botones
 var boton_codificar = document.getElementById("codificar"); // trae codificar
@@ -9,65 +8,22 @@ var boton_decodificar = document.getElementById("decodificar"); // trae codifica
 function codificar1() {
   let string = document.getElementById("entradaw").value;  // lee entrada  /// lineas deben ir dentro de funcion
   let offset = parseInt (document.getElementById("clavew").value) ; //lee clave
-  alert(cipher.encode(offset,string));
-  console.log(cipher.encode(offset,string));
+  //alert(cipher.encode(offset,string));
+  //console.log(cipher.encode(offset,string));
   document.getElementById("salida_etiq").textContent = cipher.encode(offset,string) ;/// con innerHtml tambien se puede
  }
-
-
 
  function decodificar1() {
   let string = document.getElementById("entradaw").value;  // lee entrada  /// lineas deben ir dentro de funcion
   let offset = parseInt (document.getElementById("clavew").value) ; //lee clave
-  alert(cipher.decode(offset,string));
-  console.log(cipher.decode(offset,string));
-  document.getElementById("salida_etiq").textContent = cipher.decode(offset,string) ;/// con innerHtml tambien se puede
-
-
-
-  /*   ////LEER
-let entrada = document.getElementById("entradaw").value;  // lee entrada
-let of= document.getElementById("clavew").value ;          //lee clave
-//console.log(of);
-let off= parseInt(of);
-let salida = [];
-let in_conv;
-let letra;
-
-  
- for (var i = 0; i <= entrada.length-1; i++) {  /// tratar de usar for of, cuenta hasta leng-1 xq cuenta el 0 al get size
- let in_asci= entrada.charCodeAt(i); // get #asci de cada letra de entrada
-    if (in_asci > 64 && in_asci < 91)  // evalua si mayusculas
-    {
-    in_conv = (in_asci - 65 - off) %26 + 65; // conv a nuevo alf, en otro #
-     }
-   else if (in_asci > 96 && in_asci < 123) // evalua si minusculas
-    {
-    in_conv = (in_asci - 97 - off) %26 + 97; 
-    }
-   else if (in_asci == 32 )     // evalua espacios
-    {
-    in_conv = in_asci; // mantiene espacio
-    }
-   else { // otro caso
-     in_conv = in_asci;
-   }
-   
-    letra= String.fromCharCode(in_conv); // get letra a partir numero
-    salida.push (letra);// va llenando array en cd ciclo
- }
- 
- //let mm = salida.toString(); // convierte en string (por si acaso)
- let ss = salida.join(""); //// pteeee
- //alert (ss);
- document.getElementById("salida_etiq").innerHTML = ss ; */
+  //alert(cipher.decode(offset,string));
+  //console.log(cipher.decode(offset,string));
+  document.getElementById("salida_etiq").textContent = cipher.decode(offset,string) ;/// ahí escriba ...con innerHtml tambien se puede
  }
    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
    // asigno evento y funcion a botones
    boton_codificar.addEventListener("click", codificar1 ); // lee evento click de boton codificar
    boton_decodificar.addEventListener("click", decodificar1 );
   
-   
-//event.preventDefault //
-// if entre botones   
+ 
    
