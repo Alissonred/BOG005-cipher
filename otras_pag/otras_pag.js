ebcbab51246734e27844b04e02a359aff7b41456
Alissonred <108753902+Alissonred@users.noreply.github.com>
2022-07-25 22:44:30 -0500

    version 7 cambio de reglas

--- otras_pag/otras_pag.js
+++ otras_pag/otras_pag.js
@@ -1,4 +1,92 @@
-var boton_adelante = document.getElementById("adelante"); 
+
+
+//.....................pag 4 (para mapa).....................................................................................
+let boton_grid = document.getElementById("grilla_print"); // traigo el boton
+let principal = document.getElementById("objects");
+// OPC 2 CREAMOS ARRAY CON ARRAYS QUE CONTENGAN PROP DE CU
+    var lugares =new Array();
+     lugares[0]= ["../src/imag/partes de ciudad/estacion_de_bus-removebg-preview.png", 1 ]
+     lugares[1]= ["../src/imag/partes de ciudad/banco-removebg-preview.png", 2 ]
+     lugares[2]= ["../src/imag/partes de ciudad/catedral-removebg-preview.png", 3 ]
+     lugares[3]= ["../src/imag/partes de ciudad/edificio-removebg-preview.png", 4 ]
+     lugares[4]= ["../src/imag/partes de ciudad/fabrica-removebg-preview.png", 4 ]
+     lugares[5]= ["../src/imag/partes de ciudad/hospital-removebg-preview.png", 4 ]
+     lugares[6]= ["../src/imag/partes de ciudad/hotel-removebg-preview.png", 4 ]
+     lugares[7]= ["../src/imag/partes de ciudad/iglesia-removebg-preview.png", 4 ]
+     lugares[8]= ["../src/imag/partes de ciudad/parque-removebg-preview.png", 4 ]
+     lugares[9]= ["../src/imag/partes de ciudad/tienda_1-removebg-preview.png", 4 ]
+     lugares[10]= ["../src/imag/partes de ciudad/vivienda-removebg-preview.png", 4 ]
+     lugares[11]= ["../src/imag/partes de ciudad/estacion_de_bus-removebg-preview.png", 1 ]
+     lugares[12]= ["../src/imag/partes de ciudad/hospital-removebg-preview.png", 4 ]
+     lugares[13]= ["../src/imag/partes de ciudad/hotel-removebg-preview.png", 4 ]
+     lugares[14]= ["../src/imag/partes de ciudad/iglesia-removebg-preview.png", 4 ]
+
+
+function new_set () {       
+
+        for (i=0;lugares.length;++i) {
+            let montos = Math.floor(Math.random()*(2000)+ 80);
+            lugares[i][1] = montos;
+            console.log(lugares[i][1]);
+            let creadiv = document.createElement("div") ;//// para crear etiquetas div
+            let creaimag = document.createElement("img"); // para crear etiquetas imagen
+            creadiv.setAttribute("class","foto");
+            creaimag.setAttribute("class","imagenes");
+            //lugares[i][1]+= lugares[i][1];
+            creadiv.innerHTML=lugares[i][1];
+            creaimag.setAttribute("src",lugares[i][0]); // asigna atributos a los elem creados
+            principal.appendChild(creadiv); //agrega ese elemento al final
+            principal.appendChild(creaimag); //agrega ese elemento al final
+           // creaimag.innerHTML(every_spaces[i]);
+
+        } 
+    
+}
+
+boton_grid.addEventListener("click", new_set);// asigno evento al boton
+//............................................................................................................................
+
+
+//..............de index y cipher.............................................................................................
+/* import cipher from './cipher.js'; /// traer objeto cifrado donde están las funciones ppales
+
+// traigo botones
+var boton_codificar = document.getElementById("codificar"); // trae codificar
+var boton_decodificar = document.getElementById("decodificar"); // trae codificar
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function codificar1() {
+  let string = document.getElementById("entradaw").value;  // lee entrada  /// lineas deben ir dentro de funcion
+  let offset = parseInt (document.getElementById("clavew").value) ; //lee clave
+  //alert(cipher.encode(offset,string));
+  //console.log(cipher.encode(offset,string));
+  document.getElementById("salida_etiq").textContent = cipher.encode(offset,string) ;/// con innerHtml tambien se puede
+ }
+
+ function decodificar1() {
+  let string = document.getElementById("entradaw").value;  // lee entrada  /// lineas deben ir dentro de funcion
+  let offset = parseInt (document.getElementById("clavew").value) ; //lee clave
+  //alert(cipher.decode(offset,string));
+  //console.log(cipher.decode(offset,string));
+  document.getElementById("salida_etiq").textContent = cipher.decode(offset,string) ;/// ahí escriba ...con innerHtml tambien se puede
+ }
+
+   /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   // asigno evento y funcion a botones
+   boton_codificar.addEventListener("click", codificar1 ); // lee evento click de boton codificar
+   boton_decodificar.addEventListener("click", decodificar1 );// lee evento click de boton decodificar
+
+
+ */
+
+
+
+
+
+
+/* var boton_adelante = document.getElementById("adelante"); 
 var boton_atras = document.getElementById("atras"); 
 
 function camb_adelante() {
@@ -61,4 +149,4 @@ else if(ocultar_pag5){               //si pag5 es mostrada
 
 
 boton_adelante.addEventListener("click", camb_adelante ); // lee evento click de boton adelangte
-boton_atras.addEventListener("click", camb_atras );// lee evento click de boton atras
+boton_atras.addEventListener("click", camb_atras );// lee evento click de boton atras */
